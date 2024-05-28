a9520af85e9df12723b6da416b6c5d12eaf25bab
Kseniia Bondarenko <bondarenkoksenia2007@gmail.com>
2024-05-28 20:04:20 +0300

    converter(needs further improvements)
    Signed-off-by: Kseniia Bondarenko <bondarenkoksenia2007@gmail.com>

--- src/main/java/example/ConverterController.java
+++ src/main/java/example/ConverterController.java
@@ -8,20 +8,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class ConverterController {
 
-    @Autowired
-    private ConverterService converterService;
+
+//    @Autowired
+    private ConverterService weatherService = new ConverterService();
 
     @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "index";
+    }
+
+    @PostMapping("/weather")
+    public String getWeather(@RequestParam("city") String city, Model model) {
+        Map<String, Object> weatherData = weatherService.getWeather(city);
+        model.addAttribute("city", city);
+        model.addAttribute("weatherData", weatherData);
+        return "weather";
     }
 
     @PostMapping("/convert")
     public String convert(@RequestParam("celsius") double celsius, Model model) {
-        double fahrenheit = converterService.convertToFahrenheit(celsius);
+        double fahrenheit = weatherService.convertToFahrenheit(celsius);
         model.addAttribute("celsius", celsius);
         model.addAttribute("fahrenheit", fahrenheit);
         return "result";
--- src/main/java/example/ConverterService.java
+++ src/main/java/example/ConverterService.java
@@ -1,10 +1,31 @@
 package example;
 
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @Service
 public class ConverterService {
+
+
+
+
+
+    public Map getWeather(String city) {
+        String url = UriComponentsBuilder.fromHttpUrl("https://api.openweathermap.org/data/2.5/weather")
+                .queryParam("q", city)
+                .queryParam("appid", "b0a2a0d6d05a640a93ae2499cef16636")
+                .queryParam("units", "metric")
+                .toUriString();
+        RestTemplate restTemplate = new RestTemplate();
+        Map forObject = restTemplate.getForObject(url, Map.class);
+        return forObject;
+    }
 
     public double convertToFahrenheit(double celsius) {
         return (celsius * 9/5) + 32;
